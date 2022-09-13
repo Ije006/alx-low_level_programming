bc6adf532305b95d86cbf7a097ce2c4b422af3b5
Ijem <okolugboijem@gmail.com>
2022-09-12 20:19:14 -0700

    Task - 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Print text
+ * main - Print text with the outs function
  * 
- * Return: Always 0 (Successful)
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
